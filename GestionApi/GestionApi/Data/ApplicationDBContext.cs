42980a9c9cacc084d1b61e871e8a646fe9f3e0c0
Zakariae Zbakh El Ghoury <zzbakh444@gmail.com>
2025-02-06 15:46:27 +0100

    Init

---
+++ GestionApi/GestionApi/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using GestionApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionApi.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions)
+        : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Carts> Carts { get; set; }
+    }
+}
---
+++ GestionApi/GestionApi/Models/Carts.cs
@@ -0,0 +1,9 @@
+﻿namespace GestionApi.Models
+{
+    public class Carts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Description { get; set; }
+    }
+}
