43a7b9f305f0d6f4b7de800fc0b997398b639eb3
Zakariae Zbakh El Ghoury <zzbakh444@gmail.com>
2025-02-06 15:48:23 +0100

    Wip zaka

--- GestionApi/GestionApi/obj/Debug/net8.0/GestionApi.AssemblyInfo.cs
+++ GestionApi/GestionApi/obj/Debug/net8.0/GestionApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestionApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42980a9c9cacc084d1b61e871e8a646fe9f3e0c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestionApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestionApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
